4cf2e3ca42ba158752093e906dc69bacab494876
Monitores <bispobiano13@gmail.com>
2026-06-09 22:47:54 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,13 @@ getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-D3SaEiMzTS17ElI7BBYo_R9SlHedd1I",
-  authDomain: "intervalo-musical.firebaseapp.com",
-  projectId: "intervalo-musical",
-  storageBucket: "intervalo-musical.firebasestorage.app",
-  messagingSenderId: "421542064165",
-  appId: "1:421542064165:web:b66bf05338dff19df24fcc",
-  measurementId: "G-NGLJPMMBH3"
+  apiKey: "AIzaSyB5WKVBO-qsRb5m5oVbe72aXeiC5xkorng",
+  authDomain: "intervalo-musical-dca95.firebaseapp.com",
+  projectId: "intervalo-musical-dca95",
+  storageBucket: "intervalo-musical-dca95.firebasestorage.app",
+  messagingSenderId: "61838736249",
+  appId: "1:61838736249:web:198d30e1e56fcecd118da8",
+  measurementId: "G-PE857GM66Z"
 };
 
 const app = initializeApp(firebaseConfig);
